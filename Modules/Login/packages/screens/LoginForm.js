f7c07d582952873eca4ffe8ab898d31e8c5fcc15
Ankit Raj <developer.ankitraj@gmail.com>
2020-09-20 02:26:33 +0530

    Update 2.0

--- Modules/Login/packages/screens/LoginForm.js
+++ Modules/Login/packages/screens/LoginForm.js
@@ -1,22 +1,25 @@
-import React, {useState} from 'react'
-import {View, StyleSheet, TouchableOpacity, Text} from 'react-native'
-import UserInput from '../../../UIkit/components/UserInput'
+import React, { useState } from 'react'
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native'
+
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { Button } from 'react-native-paper'
+
+import UserInput from '../../../UIkit/components/UserInput'
 
 import loginStyl from '../../assets/styles/Login.styl'
 
 const LoginForm = (props) => {
-  let [formData, setFormData] = useState({email: '', password: ''}) 
+  let [formData, setFormData] = useState({ email: '', password: '' })
 
   return (
     <>
-      <View style={{flex:1, alignItems: 'center', marginVertical: 20, justifyContent: 'flex-start'}}>
+      <View style={{ flex: 1, alignItems: 'center', marginVertical: 20, justifyContent: 'flex-start' }}>
         <UserInput
           icon={<MaterialCommunityIcons name="account" color="grey" size={26} />}
           value={formData.email}
           placeholder="Enter Email"
           placeholderTextColor="grey"
-          onChange={(email)=>setFormData({...formData, email}) }
+          onChange={(email) => setFormData({ ...formData, email })}
           textContentType="emailAddress"
         />
         <UserInput
@@ -24,18 +27,31 @@ const LoginForm = (props) => {
           value={formData.password}
           placeholder="Enter Password"
           placeholderTextColor="grey"
-          onChange={(password)=>setFormData({...formData, password}) }
+          onChange={(password) => setFormData({ ...formData, password })}
           textContentType="password"
           secureTextEntry={true}
         />
-        <TouchableOpacity style={styles.buttons}>
-          <Text style={styles.buttonTitle}>SIGN IN</Text>
-        </TouchableOpacity>
+
+        <Button
+          color={'#47c17e'}
+          dark={true}
+          style={{ margin: 10, minWidth: 150 }}
+          mode={'contained'}
+          onPress={() => alert('clicked paper')}
+        >
+          SIGN IN
+        </Button>
+
         <View style={styles.extraText}>
-          <Text>Don't have an account?</Text> 
-          <TouchableOpacity onPress={props.onFormChange}>
-            <Text style={styles.link}>Sign Up!</Text>
-          </TouchableOpacity>
+          <Text>Don't have an account?</Text>
+          <Button
+            style={{ marginLeft: 10 }}
+            dark={true}
+            color={'green'}
+            onPress={() => alert('redirect to Signup')}
+          >
+            Sign Up!
+          </Button>
         </View>
       </View>
     </>
@@ -43,35 +59,13 @@ const LoginForm = (props) => {
 }
 
 const styles = StyleSheet.create({
-  buttons: {
-    backgroundColor: `rgb(60,179,113)`,
-    height: 45,
-    width: '50%',
-    marginHorizontal: 10,
-    marginVertical: 10,
-    borderRadius: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
-    shadowColor: '#000000',
-    shadowOffset: { width: 2, height: 5 },
-    shadowOpacity: 0.5,
-    shadowRadius: 3,
-    elevation: 3
-  },
-  buttonTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#fff'
-  }, 
   extraText: {
+    marginTop: 20,
     display: 'flex',
     flexDirection: 'row',
     marginBottom: 40,
-  },
-  link: {
-    fontWeight: 'bold',
-    marginLeft: 20,
-    color: `rgb(0,100,0)`
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 })
 
--- Modules/Login/packages/screens/SignupForm.js
+++ Modules/Login/packages/screens/SignupForm.js
@@ -1,49 +1,49 @@
-import React, {useState} from 'react'
-import {View, StyleSheet, TouchableOpacity, Text} from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native'
 import UserInput from '../../../UIkit/components/UserInput'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import signupStyl from '../../assets/styles/Signup.styl'
 const SignupForm = (props) => {
-  let [formData, setFormData] = useState({email: '', password: '', confirmPassword: ''}) 
+  let [formData, setFormData] = useState({ email: '', password: '', confirmPassword: '' })
 
   return (
     <>
-      <View style={{flex:1, alignItems: 'center', marginVertical: 12}}>
-      <UserInput
-        icon={<MaterialCommunityIcons name="account-plus" color="grey" size={26} />}
-        value={formData.email}
-        placeholder="Enter Email"
-        placeholderTextColor="grey"
-        onChange={(email)=>setFormData({...formData, email}) }
-        textContentType="emailAddress"
-      />
-      <UserInput
-        icon={<MaterialCommunityIcons name="lock" color="grey" size={26} />}
-        value={formData.password}
-        placeholder="Enter Password"
-        placeholderTextColor="grey"
-        onChange={(password)=>setFormData({...formData, password}) }
-        textContentType="password"
-        secureTextEntry={true}
-      />
-      <UserInput
-        icon={<MaterialCommunityIcons name="lock-check" color="grey" size={26} />}
-        value={formData.confirmPassword}
-        placeholder="Confirm Password"
-        placeholderTextColor="grey"
-        onChange={(confirmPassword)=>setFormData({...formData, confirmPassword}) }
-        textContentType="password"
-        secureTextEntry={true}
-      />
-      <TouchableOpacity style={{...signupStyl.buttons}}>
-        <Text style={signupStyl.buttonTitle}>SIGN UP</Text>
-      </TouchableOpacity>
-      <View style={signupStyl.extraText}>
-      <Text>Already a User?</Text> 
-      <TouchableOpacity onPress={props.onFormChange}>
-        <Text style={signupStyl.link}>Sign In!</Text>
-      </TouchableOpacity>
-      </View>
+      <View style={{ flex: 1, alignItems: 'center', marginVertical: 12 }}>
+        <UserInput
+          icon={<MaterialCommunityIcons name="account-plus" color="grey" size={26} />}
+          value={formData.email}
+          placeholder="Enter Email"
+          placeholderTextColor="grey"
+          onChange={(email) => setFormData({ ...formData, email })}
+          textContentType="emailAddress"
+        />
+        <UserInput
+          icon={<MaterialCommunityIcons name="lock" color="grey" size={26} />}
+          value={formData.password}
+          placeholder="Enter Password"
+          placeholderTextColor="grey"
+          onChange={(password) => setFormData({ ...formData, password })}
+          textContentType="password"
+          secureTextEntry={true}
+        />
+        <UserInput
+          icon={<MaterialCommunityIcons name="lock-check" color="grey" size={26} />}
+          value={formData.confirmPassword}
+          placeholder="Confirm Password"
+          placeholderTextColor="grey"
+          onChange={(confirmPassword) => setFormData({ ...formData, confirmPassword })}
+          textContentType="password"
+          secureTextEntry={true}
+        />
+        <TouchableOpacity style={{ ...signupStyl.buttons }}>
+          <Text style={signupStyl.buttonTitle}>SIGN UP</Text>
+        </TouchableOpacity>
+        <View style={signupStyl.extraText}>
+          <Text>Already a User?</Text>
+          <TouchableOpacity onPress={props.onFormChange}>
+            <Text style={signupStyl.link}>Sign In!</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </>
   )
@@ -68,7 +68,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     color: '#fff'
-  }, 
+  },
   extraText: {
     flexDirection: 'row',
     marginBottom: 20,
